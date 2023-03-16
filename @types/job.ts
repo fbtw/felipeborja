7c5abb767ba6e590181e6140e3d65a34585cf162
Felipe B <piratep1@protonmail.com>
2023-03-16 17:43:31 -0300

    types

---
+++ @types/job.ts
@@ -0,0 +1,22 @@
+
+import { ICompany, IRawCompany } from '@types';
+
+export interface IJob {
+	company: ICompany;
+	jobTitle: string;
+	fromDate: string;
+	toDate: string | null;
+	technologies: string[];
+	description: string;
+}
+
+export interface IRawJob {
+	company: IRawCompany;
+	jobTitle: string;
+	fromDate: string;
+	toDate: string | null;
+	skills: Array<{ skill: string }>;
+	description: {
+		raw: string;
+	};
+}
